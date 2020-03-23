5008a3918de5a064ce084436a1510cc53ce5ece8
Ari Mahardika A.N <arimahardika.an@gmail.com>
2020-03-23 15:30:52 +0700

    Add copyright to PlayerMovements.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections;
+﻿/*
+* File name     : PlayerMovements.cs
+* Developed on  : 23/03/2020
+* Description   : Script for anything related with moving the player and 
+*                 it's animation
+* Developed by  : Ari Mahardika Ahmad Nafis
+* Contact       : arimahardika.an@gmail.com
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
